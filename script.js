b13017f34bb5a9491b51719847d8221d6ef7e685
Zachary Jordan <zachary.jordan@my.whccd.edu>
2025-10-29 21:45:43 -0700

    finish all problems

--- script.js
+++ script.js
@@ -14,3 +14,29 @@ const findCircumference = (diameter) => {
 console.log(findCircumference(20));
 
 //#########################################
+
+function firstFunction() {
+    console.log("This is the first function");
+}
+
+function secondFunction() {
+    console.log("This is the 2nd function");
+    firstFunction();
+    console.log("final console.log in other function")
+}
+
+firstFunction();
+secondFunction();
+
+
+//firstFunction is used in secondFunction, it returns the first function console log, then the 2nd function, then the first function again since it is called inside 2nd function, and then the final console.log from function2
+
+function example1() {
+    example2();
+}
+
+function example2() {
+    example1();
+}
+
+//these should cause a stack overflow since they just call eachother forever
