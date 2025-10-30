8732a0635bb9ee559d59c3aef74a118fc35d2aa8
Zachary Jordan <zacharyjordan@my.whccd.edu>
2025-10-29 18:36:44 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const myGreeting = function(name) {
+    console.log("Hello " + name);
+}
+
+console.log(myGreeting("Zach"));
+
+
+ //#######################################
+const pi = 3.14159;
+const findCircumference = (diameter) => {
+    return pi * diameter;
+};
+
+console.log(findCircumference(20));
+
+//#########################################
